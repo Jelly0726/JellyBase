43009431567c97a603a2cc8f587e78119d6f7deb
Jelly0726 <249972465@qq.com>
2018-06-13 14:26:09 +0800

    修改

---
+++ app/src/main/java/com/base/encrypt/Unicode.java
@@ -0,0 +1,49 @@
+package com.base.encrypt;
+
+/**
+ * java中文和unicode编码相互转换(转)
+ */
+public class Unicode {
+    public static void main(String[] args){
+        String s = "简介";
+        System.out.println(s+" --的unicode编码是："+ecodeUnicode(s));
+        System.out.println(ecodeUnicode(s) + " --转换成中文是："+decodeUnicode(ecodeUnicode(s)));
+    }
+
+    /*
+     * 中文转unicode编码
+     */
+    public static String ecodeUnicode(final String gbString) {
+        char[] utfBytes = gbString.toCharArray();
+        String unicodeBytes = "";
+        for (int i = 0; i < utfBytes.length; i++) {
+            String hexB = Integer.toHexString(utfBytes[i]);
+            if (hexB.length() <= 2) {
+                hexB = "00" + hexB;
+            }
+            unicodeBytes = unicodeBytes + "\\u" + hexB;
+        }
+        return unicodeBytes;
+    }
+    /*
+     * unicode编码转中文
+     */
+    public static String decodeUnicode(final String dataStr) {
+        int start = 0;
+        int end = 0;
+        final StringBuffer buffer = new StringBuffer();
+        while (start > -1) {
+            end = dataStr.indexOf("\\u", start + 2);
+            String charStr = "";
+            if (end == -1) {
+                charStr = dataStr.substring(start + 2, dataStr.length());
+            } else {
+                charStr = dataStr.substring(start + 2, end);
+            }
+            char letter = (char) Integer.parseInt(charStr, 16); // 16进制parse整形字符串。
+            buffer.append(new Character(letter).toString());
+            start = end;
+        }
+        return buffer.toString();
+    }
+}
