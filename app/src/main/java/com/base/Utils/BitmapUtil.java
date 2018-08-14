24b2fa1b228bf96e79fc5e0e627aa8a02ef6be39
Jelly0726 <249972465@qq.com>
2018-08-14 17:58:41 +0800

    修改

--- app/src/main/java/com/base/Utils/BitmapUtil.java
+++ app/src/main/java/com/base/Utils/BitmapUtil.java
@@ -251,10 +251,18 @@ public class BitmapUtil {
          */
         view.measure(measuredWidth, measuredHeight);
         view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        //无黑边全屏
+//        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+//                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+//        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
 
         view.setDrawingCacheEnabled(true);
         view.buildDrawingCache();  //启用DrawingCache并创建位图
-        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache()); //创建一个DrawingCache的拷贝，因为DrawingCache得到的位图在禁用后会被回收
+        Bitmap bitmap1=view.getDrawingCache();
+        if (bitmap1==null){
+            bitmap1=BitmapUtil.loadBitmapFromView(view, false);
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitmap1); //创建一个DrawingCache的拷贝，因为DrawingCache得到的位图在禁用后会被回收
         view.setDrawingCacheEnabled(false);  //禁用DrawingCahce否则会影响性能
         return bitmap;
     }
@@ -266,13 +274,138 @@ public class BitmapUtil {
      */
     public static Bitmap getViewBitmap(Activity activity,int layoutId) {
         View view = activity.getLayoutInflater().inflate(layoutId, null);
+//        DisplayMetrics metric = new DisplayMetrics();
+//        activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+//        int width = metric.widthPixels;     // 屏幕宽度（像素）
+//        int height = metric.heightPixels;   // 屏幕高度（像素）
+//        // 整个View的大小 参数是左上角 和右下角的坐标
+//        view.layout(0, 0, width, height);
+//        int measuredWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+//        int measuredHeight = View.MeasureSpec.makeMeasureSpec(10000, View.MeasureSpec.AT_MOST);
+//        /** 当然，measure完后，并不会实际改变View的尺寸，需要调用View.layout方法去进行布局。
+//         * 按示例调用layout函数后，View的大小将会变成你想要设置成的大小。
+//         */
+//        view.measure(measuredWidth, measuredHeight);
+//        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+        //无黑边全屏
         int me = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
         view.measure(me,me);
         view.layout(0 ,0, view.getMeasuredWidth(), view.getMeasuredHeight());
         view.buildDrawingCache();
         Bitmap bitmap = view.getDrawingCache();
+        if(bitmap==null){
+            bitmap=loadBitmapFromView(view,false);
+        }
         return bitmap;
     }
+
+    /**
+     * view转bitmap
+     * @param v
+     * @param isParemt
+     * @return
+     */
+    public static Bitmap loadBitmapFromView(View v, boolean isParemt) {
+        if (v == null) {
+            return null;
+        }
+//      Bitmap  bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(), HDConstantSet.BITMAP_QUALITY);
+        Bitmap  bitmap = Bitmap.createBitmap(v.getMeasuredWidth(), v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bitmap);
+        c.translate(-v.getScrollX(), -v.getScrollY());
+        //c.drawColor(Color.WHITE);
+        /** 如果不设置canvas画布为白色，则生成透明 */
+        v.layout(0, 0, v.getWidth(), v.getHeight());
+        v.draw(c);
+        return bitmap;
+    }
+    /**
+     * 把View绘制到Bitmap上
+     * @param comBitmap 需要绘制的View
+     * @param width 该View的宽度
+     * @param height 该View的高度
+     * @return 返回Bitmap对象
+     * add by csj 13-11-6
+     */
+    public Bitmap getViewBitmap(View comBitmap, int width, int height) {
+        Bitmap bitmap = null;
+        if (comBitmap != null) {
+            comBitmap.clearFocus();
+            comBitmap.setPressed(false);
+
+            boolean willNotCache = comBitmap.willNotCacheDrawing();
+            comBitmap.setWillNotCacheDrawing(false);
+
+            // Reset the drawing cache background color to fully transparent
+            // for the duration of this operation
+            int color = comBitmap.getDrawingCacheBackgroundColor();
+            comBitmap.setDrawingCacheBackgroundColor(0);
+            float alpha = comBitmap.getAlpha();
+            comBitmap.setAlpha(1.0f);
+
+            if (color != 0) {
+                comBitmap.destroyDrawingCache();
+            }
+
+            int widthSpec = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+            int heightSpec = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
+            comBitmap.measure(widthSpec, heightSpec);
+            comBitmap.layout(0, 0, width, height);
+
+            comBitmap.buildDrawingCache();
+            Bitmap cacheBitmap = comBitmap.getDrawingCache();
+            if (cacheBitmap == null) {
+                DebugLog.e("failed getViewBitmap(" + comBitmap + ")");
+                return null;
+            }
+            bitmap = Bitmap.createBitmap(cacheBitmap);
+            // Restore the view
+            comBitmap.setAlpha(alpha);
+            comBitmap.destroyDrawingCache();
+            comBitmap.setWillNotCacheDrawing(willNotCache);
+            comBitmap.setDrawingCacheBackgroundColor(color);
+        }
+        return bitmap;
+    }
+
+    /**
+     * 把View绘制到Bitmap上
+     * @param v
+     * @return
+     */
+    private Bitmap getViewBitmap(View v) {
+        v.clearFocus();
+        v.setPressed(false);
+
+        boolean willNotCache = v.willNotCacheDrawing();
+        v.setWillNotCacheDrawing(false);
+
+        // Reset the drawing cache background color to fully transparent
+        // for the duration of this operation
+        int color = v.getDrawingCacheBackgroundColor();
+        v.setDrawingCacheBackgroundColor(0);
+
+        if (color != 0) {
+            v.destroyDrawingCache();
+        }
+        v.buildDrawingCache();
+        Bitmap cacheBitmap = v.getDrawingCache();
+        if (cacheBitmap == null) {
+            DebugLog.e("failed getViewBitmap(" + v + ")");
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
+
+        // Restore the view
+        v.destroyDrawingCache();
+        v.setWillNotCacheDrawing(willNotCache);
+        v.setDrawingCacheBackgroundColor(color);
+
+        return bitmap;
+    }
+
     /**
      * 添加文字水印
      * @param src
