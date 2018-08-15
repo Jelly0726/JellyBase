8a22f9ccc6e6492a0281739d145f02e6f6f48352
Jelly0726 <249972465@qq.com>
2018-08-15 19:20:40 +0800

    修改

--- app/src/main/java/com/jelly/jellybase/server/WokeService.java
+++ app/src/main/java/com/jelly/jellybase/server/WokeService.java
@@ -2,8 +2,11 @@ package com.jelly.jellybase.server;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**执行工作服务
  * Created by Administrator on 2016/9/9.
@@ -22,7 +25,12 @@ public class WokeService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+//Handler将一个自定义的线程运行于主线程之上
+        new Handler(Looper.getMainLooper()).post(new Runnable(){
+            public void run(){
+                Toast.makeText(getApplicationContext(), "创建", Toast.LENGTH_LONG).show();
+            }
+        });
         return Service.START_STICKY;
     }
 
