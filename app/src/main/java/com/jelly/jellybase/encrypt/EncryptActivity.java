87ca57533d1bbbf98b4f61824dc2b2e8c163d111
Jelly0726 <18659464090@163.com>
2018-10-01 19:58:44 +0800

    更新

--- app/src/main/java/com/jelly/jellybase/encrypt/EncryptActivity.java
+++ app/src/main/java/com/jelly/jellybase/encrypt/EncryptActivity.java
@@ -1,5 +1,9 @@
 package com.jelly.jellybase.encrypt;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
@@ -7,17 +11,23 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.base.toast.ToastUtils;
 import com.jelly.jellybase.R;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-
 public class EncryptActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "body";
+
+    private static final String TEST_KEY = "JA2F8AKJF3D7HF12";
+
+    private static final String TEST_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n" + "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKLy3gcwWwy+mhcr\n" + "gcXccrRu+UQUHDwvogZbGjbBDGsyt5hY69FtwIy/45tdj42xb4Tr0o1qKjuXHmIt\n" + "zWAlgm+e9Fi4vwj6sIIbgdvYhi2dm/N2abNzEMJ2WsG2kei64qsaZtlawWv9k2GG\n" + "ChP63MR79Z9+ucBzu+SZp+XrEt9DAgMBAAECgYB4Tr51KlOfJj7YqounDWs3ItQx\n" + "WnO6UCTdcnf5QzErGIgLGGQL/W9zu92NgVeS8xV2WzLarC7AToPlUxHWUftpxqCa\n" + "alQ+HtJ2zROnbblMwmEcnwsPXD8SncjJGDg1mSxkhi/jw1riPg36Exw1VGgmww4b\n" + "+iMboCv3ApBDdxn7yQJBAM7rgodIHGf11d9+TO+PUkglc9AfCDMXQraDirU6JjCh\n" + "6AVJXH76k2oLz4DCvd3CCBcM5qGmdAzTK/X1MSToGgUCQQDJmVtKiJkPOe/N2Vi3\n" + "MkIIalnqZ9GFYtDjUV3dgI1QVgLQ8qpN2y98j8PU9nM/BpU0fU4qSX36vPCfYn0e\n" + "mS6nAkABiAKmR6VWK56Skde16iScvhI2VxRzdFedDCopny2LLJeP+nQByI7wuPen\n" + "J0nKa1Yt/X1zcsznD2UC4/aiJEmVAkACL+a8pUS71I4UdqIuwp3Sx4yYLW4pe0v2\n" + "22AgUg+2amh3adqNI66dNFYUjmPrsB+YRS++57M1MC2QHRpsZY8LAkAKFNUtX47a\n" + "4LYofojZrEdcz9O8xisB4bsv04G+WiM4bqTrlQo/6Y3YofvaP5jGSwBW8K/w6KPX\n" + "D0VGzyfqFiL7\n" + "-----END PRIVATE KEY-----\n";
+
+    private static final String TEST_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" + "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCi8t4HMFsMvpoXK4HF3HK0bvlE\n" + "FBw8L6IGWxo2wQxrMreYWOvRbcCMv+ObXY+NsW+E69KNaio7lx5iLc1gJYJvnvRY\n" + "uL8I+rCCG4Hb2IYtnZvzdmmzcxDCdlrBtpHouuKrGmbZWsFr/ZNhhgoT+tzEe/Wf\n" + "frnAc7vkmafl6xLfQwIDAQAB\n" + "-----END PUBLIC KEY-----\n";
 
     static {
         try{
-            System.loadLibrary("native-lib");
+//            System.loadLibrary("native-lib");
+            System.loadLibrary("crypto");
+            System.loadLibrary("cipher");
         }catch(Exception e){
             e.printStackTrace(System.out);
         }
@@ -26,12 +36,21 @@ public class EncryptActivity extends AppCompatActivity implements View.OnClickLi
 
     private EditText input;
     private TextView resultText;
+
+    private ClipboardManager mClipboardManager;
+    private JniUtils jni;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.encrypt_activity);
         input = (EditText)findViewById(R.id.input_ori);
         resultText = (TextView)findViewById(R.id.text_result);
+
+        mClipboardManager =(ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        jni = new JniUtils();
+
+        findViewById(R.id.btn_sha1OfApk).setOnClickListener(this);
+        findViewById(R.id.btn_apk).setOnClickListener(this);
 
         findViewById(R.id.btn_native_encode).setOnClickListener(this);
         findViewById(R.id.btn_java_encode).setOnClickListener(this);
@@ -41,108 +60,130 @@ public class EncryptActivity extends AppCompatActivity implements View.OnClickLi
 
         findViewById(R.id.btn_java_sign).setOnClickListener(this);
         findViewById(R.id.btn_native_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_md5_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_XOR_sign).setOnClickListener(this);
+        findViewById(R.id.btn_XORs_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_AES_sign).setOnClickListener(this);
+        findViewById(R.id.btn_AESs_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_RSA_sign).setOnClickListener(this);
+        findViewById(R.id.btn_RSAs_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_RSAa_sign).setOnClickListener(this);
+        findViewById(R.id.btn_RSAd_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_RSAsa_sign).setOnClickListener(this);
+        findViewById(R.id.btn_RSAsad_sign).setOnClickListener(this);
+
+        findViewById(R.id.btn_copy_sign).setOnClickListener(this);
+        findViewById(R.id.btn_past_sign).setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-
         String ori  = input.getText().toString().trim();
         String result = "";
         switch (v.getId()) {
+            case R.id.btn_sha1OfApk:
+                result ="sha1OfApk-> " + jni.sha1OfApk(this);
+                break;
+            case R.id.btn_apk:
+                result ="验证apk签名-> " + jni.verifySha1OfApk(this);
+                break;
             case R.id.btn_java_encode:
-                result = javaEncode(ori);
+                byte[] encodeByHmacSHA1 = jni.encodeByHmacSHA1(this, ori.getBytes());
+                result ="hmacSHA1签名编码->" + Base64.encodeToString(encodeByHmacSHA1, Base64.NO_WRAP);
                 break;
             case R.id.btn_native_encode:
-                result = nativeEncode(ori);
+                result = "SHA1签名->" +jni.encodeBySHA1(this,ori.getBytes());
                 break;
             case R.id.btn_java_decode:
-                result = javaDecode(ori);
+                result = "SHA224签名->" +jni.encodeBySHA224(this,ori.getBytes());
                 break;
             case R.id.btn_native_decode:
-                result = nativeDecode(ori);
+                result= "SHA256签名->" +jni.encodeBySHA256(this,ori.getBytes());
                 break;
             case R.id.btn_java_sign:
-                result = javaSign(ori);
+                result="SHA384签名->" + jni.encodeBySHA384(this,ori.getBytes());
                 break;
             case R.id.btn_native_sign:
-                result = nativeSign(ori);
+                result = "SHA512签名->" +jni.encodeBySHA512(this,ori.getBytes());
+                break;
+            case R.id.btn_md5_sign:
+                result="MD5信息摘要->" + jni.md5(this,ori.getBytes()).toUpperCase();
+                break;
+            case R.id.btn_XOR_sign:
+                result= "XOR异或加密编码->" + Base64.encodeToString(jni.xOr(this,ori.getBytes()), Base64.NO_WRAP);
+                break;
+            case R.id.btn_XORs_sign:
+                result = "XOR异或解密->" + new String(jni.xOr(this,Base64.decode(ori, Base64.NO_WRAP)));
+                break;
+            case R.id.btn_AES_sign:
+                byte[] encodeAES = jni.encodeByAES(this,TEST_KEY.getBytes(), ori.getBytes());
+                result ="AES加密编码->" + Base64.encodeToString(encodeAES, Base64.NO_WRAP);
+                break;
+            case R.id.btn_AESs_sign:
+                byte[] decodeAES = jni.decodeByAES(this,TEST_KEY.getBytes(), ori.getBytes());
+                result="AES解密->" + new String(decodeAES);
+                break;
+            case R.id.btn_RSA_sign:
+                byte[] encodeByRSAPubKey = jni.encodeByRSAPubKey(this,TEST_PUBLIC_KEY.getBytes(),
+                        ori.getBytes());
+                result ="RSA公钥加密编码->" + Base64.encodeToString(encodeByRSAPubKey, Base64.NO_WRAP);
+                break;
+            case R.id.btn_RSAs_sign:
+                byte[] decodeByRSAPrivateKey = jni.decodeByRSAPrivateKey(this,TEST_PRIVATE_KEY.getBytes(),
+                        ori.getBytes());
+                result ="RSA私钥解密->" + new String(decodeByRSAPrivateKey);
+                break;
+            case R.id.btn_RSAa_sign:
+                byte[] encodeByRSAPrivateKey = jni.encodeByRSAPrivateKey(this,TEST_PRIVATE_KEY.getBytes(),
+                        ori.getBytes());
+                result ="RSA私钥加密编码->" +  Base64.encodeToString(encodeByRSAPrivateKey, Base64.NO_WRAP);
+                break;
+            case R.id.btn_RSAd_sign:
+                byte[] decodeByRSAPubKey = jni.decodeByRSAPubKey(this,TEST_PUBLIC_KEY.getBytes(),
+                        ori.getBytes());
+                result = "RSA公钥解密->" + new String(decodeByRSAPubKey);
+                break;
+            case R.id.btn_RSAsa_sign:
+                byte[] signByRSAPrivateKey = jni.signByRSAPrivateKey(this,TEST_PRIVATE_KEY.getBytes(),
+                        ori.getBytes());
+                result="RSA私钥签名编码->" + Base64.encodeToString(signByRSAPrivateKey, Base64.NO_WRAP);
+                break;
+            case R.id.btn_RSAsad_sign:
+                byte[] signByRSAPrivateKeys = jni.signByRSAPrivateKey(this,TEST_PRIVATE_KEY.getBytes(),
+                        ori.getBytes());
+
+                int verifySign = jni.verifyByRSAPubKey(this,TEST_PUBLIC_KEY.getBytes(),
+                        ori.getBytes(), signByRSAPrivateKeys);
+                result="RSA公钥验证签名-> " + verifySign + "，1：验证成功";
+                break;
+            case R.id.btn_copy_sign:
+                String ssss=resultText.getText().toString();
+                ClipData mClipData =ClipData.newPlainText("Label", ssss);         //‘Label’这是任意文字标签
+                mClipboardManager.setPrimaryClip(mClipData);
+                ToastUtils.showShort(this, "复制成功，可以发给朋友们了。");
+                break;
+            case R.id.btn_past_sign:
+                // 粘贴板有数据，并且是文本
+                if (mClipboardManager.hasPrimaryClip()
+                        && mClipboardManager.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    ClipData.Item item = mClipboardManager.getPrimaryClip().getItemAt(0);
+                    CharSequence text = item.getText();
+                    if (text == null) {
+                        return;
+                    }
+                    input.setText(text);
+                    input.setSelection(text.length());
+                }
                 break;
         }
         resultText.setText(result);
-    }
-
-
-    private String javaEncode(String str) {
-        try {
-            byte[] key = MD5Util.toByte(getKey());
-            SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-            byte[] iv = { 49, 50, 51, 52, 49, 50, 51, 52, 49, 50, 51, 52, 49, 50, 51, 52};
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new IvParameterSpec(iv));
-            byte[] encrypted = cipher.doFinal(str.getBytes());
-
-            String base64 = Base64.encodeToString(encrypted, Base64.DEFAULT);
-            return base64;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return str;
-        }
-    }
-
-    private String javaDecode(String str) {
-        try {
-            byte[] deData = Base64.decode(str, Base64.DEFAULT);
-            byte[] key = MD5Util.toByte(getKey());
-            SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-
-            byte[] iv = { 49, 50, 51, 52, 49, 50, 51, 52, 49, 50, 51, 52, 49, 50, 51, 52};
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(iv));
-            byte[] oriData = cipher.doFinal(deData);
-            return new String(oriData);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return str;
-        }
-    }
-
-    public String javaSign(String param) {
-        String salt = getSalt();
-        String key = salt + param + salt;
-        String sign = null;
-        try {
-            sign = MD5Util.to32Str(key);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sign;
-    }
-
-    private String nativeEncode(String str) {
-        byte[] enData = SecureUtil.encryptData(str.getBytes());
-        String result = new String(Base64.encode(enData, Base64.DEFAULT));
-        return result;
-    }
-
-    private String nativeDecode(String str) {
-        byte[] deData = Base64.decode(str, Base64.DEFAULT);
-        byte[] oriData = SecureUtil.decryptData(deData);
-        String oriStr = new String(oriData);
-        return oriStr;
-    }
-
-    private String nativeSign(String param) {
-        return SecureUtil.getSign(param);
-    }
-
-    private String getKey() {
-        return "appKey" + SecureUtil.getDeviceId() + "appKey";
-    }
-
-    private String getSalt() {
-        return SecureUtil.getDeviceId() + "appKey" + SecureUtil.getDeviceId();
     }
 
 }
---
+++ app/src/main/java/com/jelly/jellybase/encrypt/JniUtils.java
@@ -0,0 +1,166 @@
+package com.jelly.jellybase.encrypt;
+
+
+import android.content.Context;
+
+public class JniUtils {
+
+    static {
+        System.loadLibrary("crypto");
+        System.loadLibrary("cipher");
+    }
+
+    /**
+     * HmacSHA1签名
+     *
+     * @param context
+     * @param src
+     * @return
+     */
+    public native byte[] encodeByHmacSHA1(Context context, byte[] src);
+
+    /**
+     * SHA1签名
+     *
+     * @param src
+     * @return
+     */
+    public native String encodeBySHA1(Context context,byte[] src);
+
+    /**
+     * SHA224签名
+     *
+     * @param src
+     * @return
+     */
+    public native String encodeBySHA224(Context context,byte[] src);
+
+    /**
+     * SHA384签名
+     *
+     * @param src
+     * @return
+     */
+    public native String encodeBySHA256(Context context,byte[] src);
+
+    /**
+     * SHA256签名
+     *
+     * @param src
+     * @return
+     */
+    public native String encodeBySHA384(Context context,byte[] src);
+
+    /**
+     * SHA512签名
+     *
+     * @param src
+     * @return
+     */
+    public native String encodeBySHA512(Context context,byte[] src);
+
+    /**
+     * AES加密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] encodeByAES(Context context,byte[] keys, byte[] src);
+
+    /**
+     * AES解密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] decodeByAES(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA公钥加密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] encodeByRSAPubKey(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA私钥解密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] decodeByRSAPrivateKey(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA私钥加密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] encodeByRSAPrivateKey(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA公钥解密
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] decodeByRSAPubKey(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA私钥签名
+     *
+     * @param keys
+     * @param src
+     * @return
+     */
+    public native byte[] signByRSAPrivateKey(Context context,byte[] keys, byte[] src);
+
+    /**
+     * RSA公钥验证签名
+     *
+     * @param keys
+     * @param src
+     * @param sign
+     * @return 1:验证成功
+     */
+    public native int verifyByRSAPubKey(Context context,byte[] keys, byte[] src, byte[] sign);
+
+    /**
+     * 异或加解密
+     *
+     * @param src
+     * @return
+     */
+    public native byte[] xOr(Context context,byte[] src);
+
+    /**
+     * MD5编码
+     *
+     * @param src
+     * @return 默认小写
+     */
+    public native String md5(Context context,byte[] src);
+
+    /**
+     * 获取apk-sha1
+     *
+     * @param context
+     * @return
+     */
+    public native String sha1OfApk(Context context);
+
+    /**
+     * 校验apk签名是否有效
+     *
+     * @param context
+     * @return
+     */
+    public native boolean verifySha1OfApk(Context context);
+}
