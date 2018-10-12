e2f4932573c10a3bf811193f0979eb6fe74ebafa
Jelly0726 <1207.qw>
2018-10-12 14:47:19 +0800

    修改

--- app/src/main/java/com/base/encrypt/MD5.java
+++ app/src/main/java/com/base/encrypt/MD5.java
@@ -99,6 +99,6 @@ public class MD5 {
         return md5StrBuff.toString();
     }
     public static void main(String[] agrs){
-         System.out.println(MD5Encode("语序HP不符合&key=758DA6688786C0D2C7"));
+         System.out.println(MD5Encode("语序HP不符合&key=758DA6688786C0D2C7").toUpperCase());
     }
 }
