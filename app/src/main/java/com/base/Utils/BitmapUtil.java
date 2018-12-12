c23f22319c2940f09f3a38995ac1a37be68d4a8f
Jelly0726 <1207.qw>
2018-12-12 11:45:45 +0800

    修改

--- app/src/main/java/com/base/Utils/BitmapUtil.java
+++ app/src/main/java/com/base/Utils/BitmapUtil.java
@@ -46,6 +46,7 @@ import android.widget.Toast;
 import com.base.appManager.BaseApplication;
 import com.base.log.DebugLog;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -151,6 +152,89 @@ public class BitmapUtil {
     private static void ScannerByMedia(Context context, String path) {
         MediaScannerConnection.scanFile(context, new String[] {path}, null, null);
         DebugLog.v("TAG", "media scanner completed");
+    }
+    /**
+     * 将byte[]转换成InputStream
+     * @param b
+     * @return
+     */
+    public InputStream Byte2InputStream(byte[] b) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        return bais;
+    }
+
+    /**
+     * 将InputStream转换成byte[]
+     * @param is
+     * @return
+     */
+    public byte[] InputStream2Bytes(InputStream is) {
+        String str = "";
+        byte[] readByte = new byte[1024];
+        int readCount = -1;
+        try {
+            while ((readCount = is.read(readByte, 0, 1024)) != -1) {
+                str += new String(readByte).trim();
+            }
+            return str.getBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将Bitmap转换成InputStream
+     * @param bm
+     * @return
+     */
+    public InputStream Bitmap2InputStream(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        return is;
+    }
+
+    /**
+     * 将Bitmap转换成InputStream
+     * @param bm
+     * @param quality
+     * @return
+     */
+    public InputStream Bitmap2InputStream(Bitmap bm, int quality) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, quality, baos);
+        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        return is;
+    }
+
+    /**
+     * 将InputStream转换成Bitmap
+     * @param is
+     * @return
+     */
+    public Bitmap InputStream2Bitmap(InputStream is) {
+        return BitmapFactory.decodeStream(is);
+    }
+
+    /**
+     * Drawable转换成InputStream
+     * @param d
+     * @return
+     */
+    public InputStream Drawable2InputStream(Drawable d) {
+        Bitmap bitmap = this.drawable2Bitmap(d);
+        return this.Bitmap2InputStream(bitmap);
+    }
+
+    /**
+     *  InputStream转换成Drawable
+     * @param is
+     * @return
+     */
+    public Drawable InputStream2Drawable(InputStream is) {
+        Bitmap bitmap = this.InputStream2Bitmap(is);
+        return this.bitmap2Drawable(bitmap);
     }
     /**
      * bitmap转字节数组
