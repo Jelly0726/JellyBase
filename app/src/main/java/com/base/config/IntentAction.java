d5a7c2754551f42960b756e0ca1f4fa2f4cca162
Jelly0726 <1207.qw>
2019-01-04 09:01:08 +0800

    修改

--- app/src/main/java/com/base/config/IntentAction.java
+++ app/src/main/java/com/base/config/IntentAction.java
@@ -7,9 +7,9 @@ import com.base.appManager.BaseApplication;
  */
 
 public class IntentAction {
-    public static final String NET_STATE="IntentAction.network.anomaly";//网络异常
-    public static final String JPUSH_CLICK="IntentAction.JPUSH_CLICK";//极光推送点击通知打开
-    public static final String NOTICE="IntentAction.NOTICE";//
+    public static final String NET_STATE=BaseApplication.getInstance().getPackageName()+".network.anomaly";//网络异常
+    public static final String JPUSH_CLICK=BaseApplication.getInstance().getPackageName()+".JPUSH_CLICK";//极光推送点击通知打开
+    public static final String NOTICE=BaseApplication.getInstance().getPackageName()+".NOTICE";//
     public static final String ACTION_MAIN= BaseApplication.getInstance().getPackageName()+".action.ACTION_MAIN";//跳转主页
     public static final String ACTION_LOGIN = BaseApplication.getInstance().getPackageName()+".action.ACTION_LOGIN";//跳转登录
     public static final String TOKEN_NOT_EXIST = BaseApplication.getInstance().getPackageName()+".action.TOKEN_NOT_EXIST";//token不存在
