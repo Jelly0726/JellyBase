755ea121f620581a0958ca12e9218fb9b35af312
Jelly0726 <1207.qw>
2019-01-11 09:46:46 +0800

    修改

--- app/src/main/java/com/base/sms/ComposeSmsActivity.java
+++ app/src/main/java/com/base/sms/ComposeSmsActivity.java
@@ -2,48 +2,13 @@ package com.base.sms;
 
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 
 public class ComposeSmsActivity extends Activity {
-    //注册监听短信数据库的变化
-    private SMSContentObserver smsContentObserver;
-    protected static final int MSG_INBOX = 1;
-    //监听到数据库变化后
-    private Handler mHandler = new Handler() {
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MSG_INBOX:
 
-                    break;
-            }
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        smsContentObserver = new SMSContentObserver(ComposeSmsActivity.this, mHandler);
-    }
-    @Override
-    protected void onResume() {
-        // TODO Auto-generated method stub
-        super.onResume();
-        if (smsContentObserver != null) {
-            getContentResolver().registerContentObserver(
-                    Uri.parse("content://sms/"), true, smsContentObserver);// 注册监听短信数据库的变化
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
-        if (smsContentObserver != null) {
-            getContentResolver().unregisterContentObserver(smsContentObserver);// 取消监听短信数据库的变化
-        }
-
     }
 }
---
+++ app/src/main/java/com/base/sms/SmsService.java
@@ -0,0 +1,56 @@
+package com.base.sms;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.annotation.Nullable;
+
+/**
+ * 监听短信数据库
+ */
+public class SmsService extends Service{
+    //注册监听短信数据库的变化
+    private SMSContentObserver smsContentObserver;
+    protected static final int MSG_INBOX = 1;
+    //监听到数据库变化后
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_INBOX:
+
+                    break;
+            }
+        }
+    };
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        smsContentObserver = new SMSContentObserver(this, mHandler);
+        if (smsContentObserver != null) {
+            getContentResolver().registerContentObserver(
+                    Uri.parse("content://sms/"), true, smsContentObserver);// 注册监听短信数据库的变化
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (smsContentObserver != null) {
+            getContentResolver().unregisterContentObserver(smsContentObserver);// 取消监听短信数据库的变化
+        }
+    }
+}
