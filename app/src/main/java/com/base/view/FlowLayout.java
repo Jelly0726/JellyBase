b4add8624f99839633d7c8b0875cdc0d77de19fc
Jelly0726 <1207.qw>
2019-06-10 16:58:58 +0800

    修改

--- app/src/main/java/com/base/view/FlowLayout.java
+++ app/src/main/java/com/base/view/FlowLayout.java
@@ -187,5 +187,33 @@ public class FlowLayout extends ViewGroup {
         }
 
     }
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MyLayoutParams(getContext(), attrs);
+    }
 
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
+        return new MyLayoutParams(lp);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MyLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    public static class MyLayoutParams extends MarginLayoutParams {
+
+        public MyLayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public MyLayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public MyLayoutParams(LayoutParams lp) {
+            super(lp);
+        }
+    }
 }
