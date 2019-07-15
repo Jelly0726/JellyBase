c0a27a55310f62eb0516b4f6ee13acc3f1eb64f8
Jelly0726 <1207.qw>
2019-07-15 11:52:55 +0800

    修改

--- app/src/main/java/com/base/Utils/ViewUtils.java
+++ app/src/main/java/com/base/Utils/ViewUtils.java
@@ -1,9 +1,12 @@
 package com.base.Utils;
 
+import android.app.Activity;
 import android.graphics.Rect;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.ObjectStreamException;
+import java.util.ArrayList;
 
 public class ViewUtils {
     private ViewUtils(){}
@@ -41,5 +44,77 @@ public class ViewUtils {
             }
         }
         return true;
+    }
+    /**
+
+     * 根据坐标获取相对应的子控件<br>
+     * 在Activity使用
+     *
+     * @param activity
+     * @param x 坐标
+     * @param y 坐标
+     * @return 目标View
+     */
+    public View getViewAtActivity(Activity activity,int x, int y) {
+        // 从Activity里获取容器
+        View root =activity.getWindow().getDecorView();
+        return findViewByXY(root, x, y);
+    }
+
+    /**
+     * 根据坐标获取相对应的子控件<br>
+     * 在重写ViewGroup使用
+     *
+     * @param x 坐标
+     * @param y 坐标
+     * @return 目标View
+     */
+    public View getViewAtViewGroup(ViewGroup viewGroup,int x, int y) {
+        return findViewByXY(viewGroup, x, y);
+    }
+
+    private View findViewByXY(View view, int x, int y) {
+        View targetView = null;
+        if (view instanceof ViewGroup) {
+            // 父容器,遍历子控件
+            ViewGroup v = (ViewGroup) view;
+            for (int i = 0; i < v.getChildCount(); i++) {
+                targetView = findViewByXY(v.getChildAt(i), x, y);
+                if (targetView != null) {
+                    break;
+                }
+            }
+        } else {
+            targetView = getTouchTarget(view, x, y);
+        }
+        return targetView;
+
+    }
+
+    private View getTouchTarget(View view, int x, int y) {
+        View targetView = null;
+        // 判断view是否可以聚焦
+        ArrayList<View> TouchableViews = view.getTouchables();
+        for (View child : TouchableViews) {
+            if (isTouchPointInView(child, x, y)) {
+                targetView = child;
+                break;
+            }
+        }
+        return targetView;
+    }
+
+    private boolean isTouchPointInView(View view, int x, int y) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int left = location[0];
+        int top = location[1];
+        int right = left + view.getMeasuredWidth();
+        int bottom = top + view.getMeasuredHeight();
+        if (view.isClickable() && y >= top && y <= bottom && x >= left
+                && x <= right) {
+            return true;
+        }
+        return false;
     }
 }
