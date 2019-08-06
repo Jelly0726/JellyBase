a9b1764fa692c11eb709522e263c66805ec01f44
Jelly0726 <1207.qw>
2019-08-06 16:11:19 +0800

    修改

--- app/src/main/java/com/base/Utils/BitmapUtil.java
+++ app/src/main/java/com/base/Utils/BitmapUtil.java
@@ -526,7 +526,28 @@ public class BitmapUtil {
         if (!src.isRecycled()) src.recycle();
         return res;
     }
-
+    /**
+     *  设置bitmap的宽高
+     *
+     * @param bm
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public static Bitmap zoomImg(Bitmap bm, int newWidth, int newHeight) {
+        // 获得图片的宽高
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        // 计算缩放比例
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // 取得想要缩放的matrix参数
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        // 得到新的图片
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        return newbm;
+    }
     /**
      * 旋转图片
      *
