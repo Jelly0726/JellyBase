98f7262ef07d06e25b355fb4fa8a9d2eab250b3d
Jelly0726 <1207.qw>
2019-08-20 18:33:43 +0800

    修改

--- app/src/main/java/com/base/httpmvp/retrofitapi/converter/MGsonRequestBodyConverter.java
+++ app/src/main/java/com/base/httpmvp/retrofitapi/converter/MGsonRequestBodyConverter.java
@@ -1,17 +1,14 @@
 package com.base.httpmvp.retrofitapi.converter;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.nio.charset.Charset;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
-import okio.Buffer;
 import retrofit2.Converter;
 
 /**
@@ -32,11 +29,11 @@ final class MGsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
 
     @Override
     public RequestBody convert(T value) throws IOException {
-        Buffer buffer = new Buffer();
-        Writer writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
-        JsonWriter jsonWriter = gson.newJsonWriter(writer);
-        adapter.write(jsonWriter, value);
-        jsonWriter.close();
-        return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
+//        Buffer buffer = new Buffer();
+//        Writer writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
+//        JsonWriter jsonWriter = gson.newJsonWriter(writer);
+//        adapter.write(jsonWriter, value);
+//        jsonWriter.close();
+        return RequestBody.create(MEDIA_TYPE, JSON.toJSONString(value));
     }
 }
