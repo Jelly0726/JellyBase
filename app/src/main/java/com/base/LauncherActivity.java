26ba8be078473e2ff3db2962d9cf8c30b702cb12
Jelly <1207.qw>
2019-10-17 13:49:49 +0800

    修改

--- app/src/main/java/com/base/LauncherActivity.java
+++ app/src/main/java/com/base/LauncherActivity.java
@@ -1,25 +1,22 @@
 package com.base;
 
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Parcelable;
 import android.view.Menu;
 
 import com.base.appManager.BaseApplication;
 import com.base.applicationUtil.AppPrefs;
+import com.base.applicationUtil.AppUtils;
 import com.base.config.ConfigKey;
 import com.base.daemon.DaemonEnv;
 import com.base.permission.CallBack;
 import com.base.permission.PermissionUtils;
 import com.base.view.BaseActivity;
 import com.jelly.jellybase.BuildConfig;
-import com.jelly.jellybase.R;
 import com.jelly.jellybase.server.TraceServiceImpl;
-import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.runtime.Permission;
 
 import hugo.weaving.DebugLog;
 
@@ -50,42 +47,64 @@ public class LauncherActivity extends BaseActivity{
 //				delShortcut();
 //			}
 		}
-		// 申请权限。
-		PermissionUtils.getInstance().requestPermission(LauncherActivity.this,new CallBack(){
-					@Override
-					public void onSucess() {
-						new Handler().postDelayed(new Runnable() {
-							public void run() {
-								if (BaseApplication.getInstance().isLogin()){
-									//BaseApplication.getInstance().goLoginActivity();
-									BaseApplication.getInstance().goMainActivity();//进入主界面
-									//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
-									finish();
-								}else{
-									if(BuildConfig.IS_MUST_LOGIN) {//是否必须登录
-										BaseApplication.getInstance().goLoginActivity();
-									}else {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			//android 6.0及以上需要动态请求权限
+			// 申请权限。
+			PermissionUtils.getInstance().requestPermission(LauncherActivity.this, new CallBack() {
+						@Override
+						public void onSucess() {
+							new Handler().postDelayed(new Runnable() {
+								public void run() {
+									if (BaseApplication.getInstance().isLogin()) {
+										//BaseApplication.getInstance().goLoginActivity();
 										BaseApplication.getInstance().goMainActivity();//进入主界面
+										//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+										finish();
+									} else {
+										if (BuildConfig.IS_MUST_LOGIN) {//是否必须登录
+											BaseApplication.getInstance().goLoginActivity();
+										} else {
+											BaseApplication.getInstance().goMainActivity();//进入主界面
+										}
+										//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+										finish();
 									}
-									//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
-									finish();
 								}
-							}
-						},1000);
+							}, 1000);
+						}
+					},
+					Permission.Group.MICROPHONE,//扩音器，麦克风
+					AppUtils.isSDcardExist()? Permission.Group.STORAGE:new String[]{},//存储
+					Permission.Group.CALENDAR,//日历
+					Permission.Group.CAMERA,//照相机
+					Permission.Group.CONTACTS,//联系人
+					Permission.Group.LOCATION,//定位
+					Permission.Group.SMS,//短信
+					new String[]{
+							android.Manifest.permission.READ_PHONE_STATE,//读取手机状态
+							android.Manifest.permission.CALL_PHONE,//拨打电话
+							android.Manifest.permission.SYSTEM_ALERT_WINDOW//<!-- 显示系统窗口权限 -->
+					});
+		}else {
+			new Handler().postDelayed(new Runnable() {
+				public void run() {
+					if (BaseApplication.getInstance().isLogin()) {
+						//BaseApplication.getInstance().goLoginActivity();
+						BaseApplication.getInstance().goMainActivity();//进入主界面
+						//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+						finish();
+					} else {
+						if (BuildConfig.IS_MUST_LOGIN) {//是否必须登录
+							BaseApplication.getInstance().goLoginActivity();
+						} else {
+							BaseApplication.getInstance().goMainActivity();//进入主界面
+						}
+						//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+						finish();
 					}
-				},
-				Permission.Group.MICROPHONE,//扩音器，麦克风
-				Permission.Group.STORAGE,//存储
-				Permission.Group.CALENDAR,//日历
-				Permission.Group.CAMERA,//照相机
-				Permission.Group.CONTACTS,//联系人
-				Permission.Group.LOCATION,//定位
-				Permission.Group.SMS,//短信
-				new String[]{
-						android.Manifest.permission.READ_PHONE_STATE,//读取手机状态
-						android.Manifest.permission.CALL_PHONE,//拨打电话
-						android.Manifest.permission.SYSTEM_ALERT_WINDOW//<!-- 显示系统窗口权限 -->
-				});
+				}
+			}, 1000);
+		}
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,68 +112,7 @@ public class LauncherActivity extends BaseActivity{
 		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	//检测是否存在快捷方式
-	private boolean hasShortcut()
-	{
-		boolean isInstallShortcut = false;
-		try{
-			final ContentResolver cr = getContentResolver();
-			final Uri CONTENT_URI = Uri.parse("content://com.android.launcher2.settings/favorites?notify=true");//保持默认
-			Cursor c = cr.query(CONTENT_URI,new String[] {"title","iconResource" },"title=?", //保持默认
-					//getString(R.string.app_name)是获取string配置文件中的程序名字，这里用一个String的字符串也可以
-					new String[] {getString(R.string.app_name).trim()}, null);
-			if(c!=null && c.getCount()>0){
-				isInstallShortcut = true ;
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return isInstallShortcut ;
-	}
-	/**
-	 * 创建桌面快捷方式
-	 */
-	public void createShut() {
-		try{
-			// 创建添加快捷方式的Intent
-			Intent addIntent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-			String title = getResources().getString(R.string.app_name);
-			// 加载快捷方式的图标
-			Parcelable icon = Intent.ShortcutIconResource.fromContext(
-					LauncherActivity.this, R.mipmap.ic_launcher);
-			// 创建点击快捷方式后操作Intent,该处当点击创建的快捷方式后，再次启动该程序
-			Intent myIntent = new Intent(LauncherActivity.this,
-					LauncherActivity.class);
-			// 设置快捷方式的标题
-			addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, title);
-			// 设置快捷方式的图标
-			addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
-			// 设置快捷方式对应的Intent
-			addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, myIntent);
-			addIntent.putExtra("duplicate", false); //不允许重复创建
-			// 发送广播添加快捷方式
-			sendBroadcast(addIntent);
-			AppPrefs.putBoolean(BaseApplication.getInstance(),ConfigKey.FIRST,false);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	//删除快捷方式
-	private void delShortcut(){
-		try{
-			Intent shortcut = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
-			//快捷方式的名称
-			shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
 
-			//这里的intent要和创建时的intent设置一致
-			Intent intent = new Intent(this,LauncherActivity.class);
-			intent.setAction("com.hooypay.Activity.logo");
-			shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
-			sendBroadcast(shortcut);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
 	@Override
 	protected void onResume() {
 		super.onResume();
--- app/src/main/java/com/base/applicationUtil/AppUtils.java
+++ app/src/main/java/com/base/applicationUtil/AppUtils.java
@@ -7,6 +7,7 @@ import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -14,8 +15,12 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
 import android.content.pm.Signature;
+import android.database.Cursor;
 import android.graphics.Point;
+import android.graphics.drawable.Icon;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.net.ConnectivityManager;
@@ -26,6 +31,7 @@ import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Parcelable;
 import android.os.StatFs;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.FileProvider;
@@ -40,8 +46,12 @@ import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.base.LauncherActivity;
 import com.base.appManager.BaseApplication;
+import com.base.config.ConfigKey;
 import com.base.encrypt.MD5;
+import com.jelly.jellybase.R;
+import com.jelly.jellybase.receiver.PhoneStatReceiver;
 
 import org.apache.commons.beanutils.ConvertUtilsBean;
 
@@ -1311,6 +1321,99 @@ public class AppUtils {
             method.setAccessible(true);
             method.invoke(fragmentManager);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //检测是否存在快捷方式
+    private static boolean hasShortcut()
+    {
+        boolean isInstallShortcut = false;
+        try{
+            final ContentResolver cr = BaseApplication.getInstance().getContentResolver();
+            final Uri CONTENT_URI = Uri.parse("content://com.android.launcher2.settings/favorites?notify=true");//保持默认
+            Cursor c = cr.query(CONTENT_URI,new String[] {"title","iconResource" },"title=?", //保持默认
+                    //getString(R.string.app_name)是获取string配置文件中的程序名字，这里用一个String的字符串也可以
+                    new String[] {BaseApplication.getInstance().getString(R.string.app_name).trim()}, null);
+            if(c!=null && c.getCount()>0){
+                isInstallShortcut = true ;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return isInstallShortcut ;
+    }
+    /**
+     * 创建桌面快捷方式
+     * 8.0以下手机提示应用未安装
+     * 在AndroidManifest文件里快捷方式启动的页面acitivity标签里添加android:exported="true"
+     * 8.0系统点击创建快捷方式无反应
+     * 将应用权限列表中的 创建桌面快捷方式 权限打开
+     */
+    public static void createShut() {
+        if (hasShortcut()){return;}
+        try{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+                //获取shortcutManager
+                ShortcutManager shortcutManager = (ShortcutManager) BaseApplication.getInstance()
+                        .getSystemService(Context.SHORTCUT_SERVICE);
+                //如果默认桌面支持requestPinShortcut（ShortcutInfo，IntentSender）方法，则返回TRUE。
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//8.0之后版本
+                    if(shortcutManager != null && shortcutManager.isRequestPinShortcutSupported()){
+                        Intent shortCutIntent = new Intent(BaseApplication.getInstance(),LauncherActivity.class);//快捷方式启动页面
+                        shortCutIntent.setAction(Intent.ACTION_VIEW);
+                        //快捷方式创建相关信息。图标名字 id
+                        ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(BaseApplication.getInstance(),"shortcutid")
+                                .setIcon(Icon.createWithResource(BaseApplication.getInstance(),R.drawable.ic_launcher))
+                                .setShortLabel(BaseApplication.getInstance().getResources().getString(R.string.app_name))
+                                .setIntent(shortCutIntent)
+                                .build();
+                        //创建快捷方式时候回调
+                        PendingIntent pendingIntent = PendingIntent.getBroadcast(BaseApplication.getInstance(),0,new
+                                Intent(BaseApplication.getInstance(), PhoneStatReceiver.class),PendingIntent.FLAG_UPDATE_CURRENT);
+                        shortcutManager.requestPinShortcut(shortcutInfo,pendingIntent.getIntentSender());
+                        AppPrefs.putBoolean(BaseApplication.getInstance(), ConfigKey.FIRST,false);
+                        return;
+                    }
+                }
+
+            }
+            //8.0以前版本
+            // 创建添加快捷方式的Intent
+            Intent addIntent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+            String title = BaseApplication.getInstance().getResources().getString(R.string.app_name);
+            // 加载快捷方式的图标
+            Parcelable icon = Intent.ShortcutIconResource.fromContext(
+                    BaseApplication.getInstance(), R.mipmap.ic_launcher);
+            // 创建点击快捷方式后操作Intent,该处当点击创建的快捷方式后，再次启动该程序
+            Intent myIntent = new Intent(BaseApplication.getInstance(),
+                    LauncherActivity.class);
+            myIntent.addCategory(Intent.CATEGORY_LAUNCHER);//添加categoryCATEGORY_LAUNCHER 应用被卸载
+            // 设置快捷方式的标题
+            addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, title);
+            // 设置快捷方式的图标
+            addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+            // 设置快捷方式对应的Intent
+            addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, myIntent);
+            addIntent.putExtra("duplicate", false); //不允许重复创建
+            // 发送广播添加快捷方式
+            BaseApplication.getInstance().sendBroadcast(addIntent);
+            AppPrefs.putBoolean(BaseApplication.getInstance(), ConfigKey.FIRST,false);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    //删除快捷方式
+    public static void delShortcut(){
+        try{
+            Intent shortcut = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
+            //快捷方式的名称
+            shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, BaseApplication.getInstance().getString(R.string.app_name));
+            //这里的intent要和创建时的intent设置一致
+            Intent intent = new Intent(BaseApplication.getInstance(),LauncherActivity.class);
+            intent.setAction("com.hooypay.Activity.logo");
+            shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+            BaseApplication.getInstance().sendBroadcast(shortcut);
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
