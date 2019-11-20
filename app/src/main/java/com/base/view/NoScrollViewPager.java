c1109a467a3d57aa0e183c710457766a4dd94b34
Jelly <1207.qw>
2019-11-20 15:59:20 +0800

    修改

--- app/src/main/java/com/base/view/NoScrollViewPager.java
+++ app/src/main/java/com/base/view/NoScrollViewPager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * 禁止viewpager页面的滑动的效果
@@ -37,7 +38,21 @@ public class NoScrollViewPager extends ViewPager {
     public void scrollTo(int x, int y) {
         super.scrollTo(x, y);
     }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height)
+                height = h;
+        }
 
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
     @Override
     public boolean onTouchEvent(MotionEvent arg0) {
                 /* return false;//super.onTouchEvent(arg0); */
