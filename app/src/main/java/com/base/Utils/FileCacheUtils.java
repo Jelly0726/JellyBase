e91b02f6185e76ee790e2abca5ea3435c35efa92
Jelly <1207.qw>
2019-12-05 15:19:42 +0800

    修改

--- app/src/main/java/com/base/Utils/FileCacheUtils.java
+++ app/src/main/java/com/base/Utils/FileCacheUtils.java
@@ -205,7 +205,7 @@ public class FileCacheUtils {
     public static String getFormatSize(double size) {
         double kiloByte = size / 1024;
         if (kiloByte < 1) {
-            return size + "Byte";
+            return size + "B";
         }
 
         double megaByte = kiloByte / 1024;
