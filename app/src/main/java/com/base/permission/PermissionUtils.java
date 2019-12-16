6ab4d37c5f6ea2377d4cd32601b086ed0c758fce
Jelly <1207.qw>
2019-12-16 16:15:06 +0800

    修改

--- app/src/main/java/com/base/permission/PermissionUtils.java
+++ app/src/main/java/com/base/permission/PermissionUtils.java
@@ -1,7 +1,6 @@
 package com.base.permission;
 
 import android.app.AlertDialog;
-import android.app.AppOpsManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -12,15 +11,13 @@ import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.widget.Toast;
 
-import com.base.appManager.BaseApplication;
 import com.jelly.jellybase.R;
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
-import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.runtime.Permission;
 
 import java.io.ObjectStreamException;
 import java.util.ArrayList;
@@ -35,7 +32,6 @@ import static android.content.Context.POWER_SERVICE;
 public class PermissionUtils {
     //设置请求码
     public static final int REQUEST_CODE_SETTING = 300;
-    public static final int RC_PERMISSION_MANAGE_OVERLAY_PERMISSION = 301;
     private static final String SHOW_DOZE_ALERT_KEY = "SHOW_DOZE_ALERT_KEY";
     private PermissionUtils(){
     }
@@ -70,14 +66,25 @@ public class PermissionUtils {
                 .onGranted(new Action<List<String>>() {
                     @Override
                     public void onAction(List<String> permissions) {
-                        Toast.makeText(context, R.string.permission_successfully, Toast.LENGTH_SHORT).show();
                         if (callBack!=null)
-                        callBack.onSucess();
+                            callBack.onSucess();
                     }
                 })
                 .onDenied(new Action<List<String>>() {
                     @Override
                     public void onAction(@NonNull List<String> permissions) {
+                        List<String> perm=new ArrayList<>();
+                        perm.addAll(permissions);
+                        for (int i=0;i<perm.size();i++) {
+                            if (perm.get(i).contains("CAMERA")
+                                    ||perm.get(i).contains("STORAGE"))
+                                permissions.remove(i);
+                        }
+                        if (permissions.size()<=0){
+                            if (callBack!=null)
+                                callBack.onSucess();
+                            return;
+                        }
                         Toast.makeText(context, R.string.permission_failure, Toast.LENGTH_SHORT).show();
                         if (callBack!=null)
                             callBack.onFailure(permissions);
@@ -99,14 +106,25 @@ public class PermissionUtils {
                 .onGranted(new Action<List<String>>() {
                     @Override
                     public void onAction(List<String> permissions) {
-                        Toast.makeText(context, R.string.permission_successfully, Toast.LENGTH_SHORT).show();
                         if (callBack!=null)
-                        callBack.onSucess();
+                            callBack.onSucess();
                     }
                 })
                 .onDenied(new Action<List<String>>() {
                     @Override
                     public void onAction(@NonNull List<String> permissions) {
+                        List<String> perm=new ArrayList<>();
+                        perm.addAll(permissions);
+                        for (int i=0;i<perm.size();i++) {
+                            if (perm.get(i).contains("CAMERA")
+                                    ||perm.get(i).contains("STORAGE"))
+                                permissions.remove(i);
+                        }
+                        if (permissions.size()<=0){
+                            if (callBack!=null)
+                                callBack.onSucess();
+                            return;
+                        }
                         Toast.makeText(context, R.string.permission_failure, Toast.LENGTH_SHORT).show();
                         if (callBack!=null)
                             callBack.onFailure(permissions);
@@ -148,7 +166,7 @@ public class PermissionUtils {
         AndPermission.with(context)
                 .runtime()
                 .setting()
-                .start(REQUEST_CODE_SETTING);
+                .start(0);
     }
     //电池优化白名单
     private AlertDialog alertDialog;
@@ -207,38 +225,10 @@ public class PermissionUtils {
                         .create();
             try {
                 if (alertDialog!=null)
-                if (!alertDialog.isShowing())
-                    alertDialog.show();
+                    if (!alertDialog.isShowing())
+                        alertDialog.show();
             } catch (Throwable ignored) {
                 ignored.printStackTrace();
-            }
-        }
-    }
-    /**
-     * 判断系统弹出权限是否开启
-     * @param context
-     * @return
-     */
-    public void requestAlertWindowPermission(Context context) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            AppOpsManager appOpsMgr = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-            int mode = appOpsMgr.checkOpNoThrow("android:system_alert_window", android.os.Process.myUid(), context.getPackageName());
-            if (mode ==2){
-                List<String> permissions=new ArrayList<String>();
-                permissions.add("开启悬浮窗");
-                showSettingDialog(context,permissions);
-            }
-        }
-
-    }
-    /**
-     * 悬浮窗权限
-     */
-    public void askAlertWindowPermission(Context context){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(BaseApplication.getInstance())) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" +context.getPackageName()));
-                ((AppCompatActivity)context).startActivityForResult(intent, RC_PERMISSION_MANAGE_OVERLAY_PERMISSION);
             }
         }
     }
