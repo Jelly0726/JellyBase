ebcb532f0156d66d85392f9dc621e0e09f082989
Jelly <1207.qw>
2020-09-29 10:44:54 +0800

    修改

--- app/src/main/java/com/jelly/jellybase/datamodel/Coupon.kt
+++ app/src/main/java/com/jelly/jellybase/datamodel/Coupon.kt
@@ -1,8 +1,17 @@
-package com.jelly.jellybase.datamodel;
+package com.jelly.jellybase.datamodel
+
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by Administrator on 2017/10/17.
  */
+@SuppressLint("ParcelCreator")// 用于处理 Lint 的错误提示
+@Parcelize
+data class Coupon(
+        var id:String="",//ID
+        var name:String=""//名称
+): Parcelable{
 
-public class Coupon {
 }
