a6139e8b995e4bc7748a8a2b40e2898101806423
Jelly <1207.qw>
2020-10-13 11:43:06 +0800

    修改

--- app/src/main/java/com/base/LauncherActivity.java
+++ app/src/main/java/com/base/LauncherActivity.java
@@ -25,85 +25,118 @@ import java.util.List;
 import hugo.weaving.DebugLog;
 
 @DebugLog
-public class LauncherActivity extends BaseActivity{
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		//判断新生成启动活动是否在栈底,否:弹出,恢复上一次活动
-		if (!isTaskRoot()){
-			Intent intent = getIntent();
-			String action = intent.getAction();
-			if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && action != null && action.equals(Intent.ACTION_MAIN)){
-				finish();
-				return;
-			}
-		}
-		//下面为纯色背景启动图标  若使用背景图片(styles)注释下面即可
+public class LauncherActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //判断新生成启动活动是否在栈底,否:弹出,恢复上一次活动
+        if (!isTaskRoot()) {
+            Intent intent = getIntent();
+            String action = intent.getAction();
+            if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && action != null && action.equals(Intent.ACTION_MAIN)) {
+                finish();
+                return;
+            }
+        }
+        //下面为纯色背景启动图标  若使用背景图片(styles)注释下面即可
 //		setContentView(R.layout.base_activity_launcher);
-		//开启服务//true  表示停止服务，false  表示启动服务
-		TraceServiceImpl.sShouldStopService = true;
-		DaemonEnv.startServiceMayBind(TraceServiceImpl.class);
+        //开启服务//true  表示停止服务，false  表示启动服务
+        TraceServiceImpl.sShouldStopService = true;
+        DaemonEnv.startServiceMayBind(TraceServiceImpl.class);
 
-		if (AppPrefs.getBoolean(BaseApplication.getInstance(),ConfigKey.FIRST,true)) {
+        if (AppPrefs.getBoolean(BaseApplication.getInstance(), ConfigKey.FIRST, true)) {
 //			if(!hasShortcut()){
 //				createShut();// 创建快捷方式
 //			}else{
 //				delShortcut();
 //			}
-		}
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			//android 6.0及以上需要动态请求权限
-			if (AppPrefs.getBoolean(BaseApplication.getInstance(), ConfigKey.FIRST,true)) {//第一次运行
-				PrivacyDialog privacyDialog = PrivacyDialog.getInstance();
-				privacyDialog.setOnClickListener(new PrivacyDialog.OnClickListener() {
-					@Override
-					public void onAgree() {
-						// 申请权限。
-						requestPermission();
-						AppPrefs.putBoolean(BaseApplication.getInstance(),ConfigKey.FIRST,false);
-					}
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //android 6.0及以上需要动态请求权限
+            if (AppPrefs.getBoolean(BaseApplication.getInstance(), ConfigKey.FIRST, true)) {//第一次运行
+                PrivacyDialog privacyDialog = PrivacyDialog.getInstance();
+                privacyDialog.setOnClickListener(new PrivacyDialog.OnClickListener() {
+                    @Override
+                    public void onAgree() {
+                        // 申请权限。
+                        requestPermission();
+                        AppPrefs.putBoolean(BaseApplication.getInstance(), ConfigKey.FIRST, false);
+                    }
 
-					@Override
-					public void onRefuse() {
-						AppSubject.getInstance().exit();
-					}
-				});
-				privacyDialog.show(getSupportFragmentManager(), "PrivacyDialog");
-			}else {
-				// 申请权限。
-				requestPermission();
-			}
-		}else {
-			new Handler().postDelayed(new Runnable() {
-				public void run() {
-					if (BaseApplication.getInstance().isLogin()) {
-						//BaseApplication.getInstance().goLoginActivity();
-						BaseApplication.getInstance().goMainActivity();//进入主界面
-						//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
-						finish();
-					} else {
-						if (BuildConfig.IS_MUST_LOGIN) {//是否必须登录
-							BaseApplication.getInstance().goLoginActivity();
-						} else {
-							BaseApplication.getInstance().goMainActivity();//进入主界面
-						}
-						//BaseApplication.getInstance().goGuideActivity();//进入引导页界面
-						finish();
-					}
-				}
-			}, 1000);
-		}
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	private void requestPermission(){
-		PermissionUtils.getInstance().requestPermission(LauncherActivity.this, new CallBack() {
-					@Override
-					public void onSucess() {
+                    @Override
+                    public void onRefuse() {
+                        AppSubject.getInstance().exit();
+                    }
+                });
+                privacyDialog.show(getSupportFragmentManager(), "PrivacyDialog");
+            } else {
+                // 申请权限。
+                requestPermission();
+            }
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    if (BaseApplication.getInstance().isLogin()) {
+                        //BaseApplication.getInstance().goLoginActivity();
+                        BaseApplication.getInstance().goMainActivity();//进入主界面
+                        //BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+                        finish();
+                    } else {
+                        if (BuildConfig.IS_MUST_LOGIN) {//是否必须登录
+                            BaseApplication.getInstance().goLoginActivity();
+                        } else {
+                            BaseApplication.getInstance().goMainActivity();//进入主界面
+                        }
+                        //BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+                        finish();
+                    }
+                }
+            }, 1000);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void requestPermission() {
+        PermissionUtils.getInstance().requestPermission(LauncherActivity.this, new CallBack() {
+                    @Override
+                    public void onSucess() {
+                        new Handler().postDelayed(new Runnable() {
+                            public void run() {
+                                if (BaseApplication.getInstance().isLogin()) {
+                                    //BaseApplication.getInstance().goLoginActivity();
+                                    BaseApplication.getInstance().goMainActivity();//进入主界面
+                                    //BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+                                    finish();
+                                } else {
+                                    if (BuildConfig.IS_MUST_LOGIN) {//是否必须登录
+                                        BaseApplication.getInstance().goLoginActivity();
+                                    } else {
+                                        BaseApplication.getInstance().goMainActivity();//进入主界面
+                                    }
+                                    //BaseApplication.getInstance().goGuideActivity();//进入引导页界面
+                                    finish();
+                                }
+                            }
+                        }, 1000);
+                    }
+
+                    @Override
+                    public void onFailure(List<String> permissions) {
+                        StringBuffer msg = new StringBuffer();
+                        for (String permission : permissions) {
+                            msg.append(permission);
+                            msg.append("\n");
+                        }
+                        if (msg.length() > 0) {
+                            ToastUtils.showShort(LauncherActivity.this, msg.toString());
+                            return;
+                        }
 						new Handler().postDelayed(new Runnable() {
 							public void run() {
 								if (BaseApplication.getInstance().isLogin()) {
@@ -122,46 +155,36 @@ public class LauncherActivity extends BaseActivity{
 								}
 							}
 						}, 1000);
-					}
-
-					@Override
-					public void onFailure(List<String> permissions) {
-						StringBuffer msg=new StringBuffer();
-						for (String permission : permissions) {
-							msg.append(permission);
-							msg.append("\n");
-						}
-
-						ToastUtils.showShort(LauncherActivity.this, msg.toString());
-					}
-				},
-				Permission.Group.MICROPHONE,//扩音器，麦克风
-				Permission.Group.STORAGE,//存储
-				Permission.Group.CALENDAR,//日历
-				Permission.Group.CAMERA,//照相机
+                    }
+                },
+                Permission.Group.MICROPHONE,//扩音器，麦克风
+                Permission.Group.STORAGE,//存储
+                Permission.Group.CALENDAR,//日历
+                Permission.Group.CAMERA,//照相机
 //					Permission.Group.CONTACTS,//联系人
 //				Permission.Group.LOCATION,//定位 部分手机设置为仅使用时返回授权失败所以在开启定位前再请求授权
-				Permission.Group.SMS,//短信
-				new String[]{
-						Permission.READ_PHONE_STATE,//读取手机状态
-						Permission.CALL_PHONE,//拨打电话
-						android.Manifest.permission.SYSTEM_ALERT_WINDOW//<!-- 显示系统窗口权限 -->
-				});
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
+                Permission.Group.SMS,//短信
+                new String[]{
+                        Permission.READ_PHONE_STATE,//读取手机状态
+                        Permission.CALL_PHONE,//拨打电话
+                        android.Manifest.permission.SYSTEM_ALERT_WINDOW//<!-- 显示系统窗口权限 -->
+                });
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
