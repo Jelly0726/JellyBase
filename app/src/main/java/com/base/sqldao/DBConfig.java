3b1d91886eb92d4f1a5f1ae2fb6e3e2c2710ad02
Jelly <1207.qw>
2021-05-14 16:08:23 +0800

    优化

--- app/src/main/java/com/base/sqldao/DBConfig.java
+++ app/src/main/java/com/base/sqldao/DBConfig.java
@@ -1,9 +1,11 @@
 package com.base.sqldao;
 
+import com.jelly.jellybase.BuildConfig;
+
 /**
  * Created by Administrator on 2017/9/7.
  */
 
 public class DBConfig {
-    public static final String DBNAME = "Jelly_DB";//数据库名称
+    public static final String DBNAME = BuildConfig.APPLICATION_ID.replace(".", "_")+".db";//数据库名称
 }
