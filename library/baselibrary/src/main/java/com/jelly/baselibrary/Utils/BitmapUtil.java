cce6eccb242a3cdd5bf3d6984c04108ab2f42828
Jelly <1207.qw>
2021-06-29 18:36:29 +0800

    优化

--- library/baselibrary/src/main/java/com/jelly/baselibrary/Utils/BitmapUtil.java
+++ library/baselibrary/src/main/java/com/jelly/baselibrary/Utils/BitmapUtil.java
@@ -55,6 +55,7 @@ import com.jelly.baselibrary.applicationUtil.AppUtils;
 import com.jelly.baselibrary.cropper.CropperActivity;
 import com.jelly.baselibrary.log.LogUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -766,8 +767,14 @@ public class BitmapUtil {
         byte[] b = baos.toByteArray();
         return Base64.encodeToString(b, Base64.DEFAULT);
     }
-
-    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    /**
+     * 计算压缩比
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public int calculateInSampleSize(BitmapFactory.Options options, float reqWidth, float reqHeight) {
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
@@ -1872,7 +1879,141 @@ public class BitmapUtil {
         }
 
     }
+    /**
+     * 压缩图片
+     * 不能压缩回收的位图
+     * @param srcImagePath    图片的路径
+     * @param outWidth        期望的输出图片的宽度
+     * @param outHeight       期望的输出图片的高度
+     * @param maxFileSize     期望的输出图片的最大占用的存储空间
+     * @return
+     */
+    public String compressImage(String srcImagePath, int outWidth, int outHeight, int maxFileSize) {
 
+        //进行大小缩放来达到压缩的目的
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(srcImagePath, options);
+        //根据原始图片的宽高比和期望的输出图片的宽高比计算最终输出的图片的宽和高
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+        float maxWidth = outWidth;
+        float maxHeight = outHeight;
+        float srcRatio = srcWidth / srcHeight;
+        float outRatio = maxWidth / maxHeight;
+        float actualOutWidth = srcWidth;
+        float actualOutHeight = srcHeight;
+
+        if (srcWidth > maxWidth || srcHeight > maxHeight) {
+            //如果输入比率小于输出比率,则最终输出的宽度以maxHeight为准()
+            //比如输入比为10:20 输出比是300:10 如果要保证输出图片的宽高比和原始图片的宽高比相同,则最终输出图片的高为10
+            //宽度为10/20 * 10 = 5  最终输出图片的比率为5:10 和原始输入的比率相同
+
+            //同理如果输入比率大于输出比率,则最终输出的高度以maxHeight为准()
+            //比如输入比为20:10 输出比是5:100 如果要保证输出图片的宽高比和原始图片的宽高比相同,则最终输出图片的宽为5
+            //高度需要根据输入图片的比率计算获得 为5 / 20/10= 2.5  最终输出图片的比率为5:2.5 和原始输入的比率相同
+            if (srcRatio < outRatio) {
+                actualOutHeight = maxHeight;
+                actualOutWidth = actualOutHeight * srcRatio;
+            } else if (srcRatio > outRatio) {
+                actualOutWidth = maxWidth;
+                actualOutHeight = actualOutWidth / srcRatio;
+            } else {
+                actualOutWidth = maxWidth;
+                actualOutHeight = maxHeight;
+            }
+        }
+        options.inSampleSize = calculateInSampleSize(options, actualOutWidth, actualOutHeight);
+        options.inJustDecodeBounds = false;
+        Bitmap scaledBitmap = null;
+        try {
+            scaledBitmap = BitmapFactory.decodeFile(srcImagePath, options);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+        }
+        if (scaledBitmap == null) {
+            return null;//压缩失败
+        }
+        //生成最终输出的bitmap
+        Bitmap actualOutBitmap = Bitmap.createScaledBitmap(scaledBitmap, (int) actualOutWidth, (int) actualOutHeight, true);
+        if(actualOutBitmap != scaledBitmap)
+            scaledBitmap.recycle();
+
+        //处理图片旋转问题
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(srcImagePath);
+            int orientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION, 0);
+            Matrix matrix = new Matrix();
+            if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                matrix.postRotate(180);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                matrix.postRotate(270);
+            }
+            actualOutBitmap = Bitmap.createBitmap(actualOutBitmap, 0, 0,
+                    actualOutBitmap.getWidth(), actualOutBitmap.getHeight(), matrix, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        //进行有损压缩
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int options_ = 100;
+        actualOutBitmap.compress(Bitmap.CompressFormat.JPEG, options_, baos);//质量压缩方法，把压缩后的数据存放到baos中 (100表示不压缩，0表示压缩到最小)
+
+        int baosLength = baos.toByteArray().length;
+
+        while (baosLength / 1024 > maxFileSize) {//循环判断如果压缩后图片是否大于maxMemmorrySize,大于继续压缩
+            baos.reset();//重置baos即让下一次的写入覆盖之前的内容
+            options_ = Math.max(0, options_ - 10);//图片质量每次减少10
+            actualOutBitmap.compress(Bitmap.CompressFormat.JPEG, options_, baos);//将压缩后的图片保存到baos中
+            baosLength = baos.toByteArray().length;
+            if (options_ == 0)//如果图片的质量已降到最低则，不再进行压缩
+                break;
+        }
+        actualOutBitmap.recycle();
+
+        //将bitmap保存到指定路径
+        FileOutputStream fos = null;
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "Cache");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String bitName = System.currentTimeMillis() + ".jpg";
+        String filePath = (file.getAbsolutePath() + File.separator + bitName);
+        try {
+            fos = new FileOutputStream(filePath);
+            //包装缓冲流,提高写入速度
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fos);
+            bufferedOutputStream.write(baos.toByteArray());
+            bufferedOutputStream.flush();
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        } finally {
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return filePath;
+    }
     /**
      * 获取图片宽高回调
      */
